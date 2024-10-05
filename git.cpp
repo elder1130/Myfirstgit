807c4bffb89cf3794c9d4e91d4eedeb913941c97
elder1130 <elder1130@naver.com>
2024-10-05 17:35:53 +0900

    sending this to my second reopsitory

---
+++ git.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is the  other one!" << endl;
+	int num1;
+	cin >> num1;
+
+	int num2 = 100;
+	double result;
+	result = (double)num1 * (double)num2;
+	cout << "result is : " << result << endl;
+
+	return 0;
+}
